c39095d57d58dcc6678737cf6143793d0d908f58
wjh <2296635996@qq.com>
2020-12-20 14:54:31 +0800

    第一次向github提交代码

---
+++ index.py
@@ -0,0 +1,39 @@
+# print("python") # 字符串
+# print(123) # 整数
+# print(True) # 布尔值
+# print(()) # 元组
+# print([]) # 数组
+# print({}) # 字典
+# print("python"+"haha") # 字符串的拼接
+# print("python"*10) # 字符串的重复
+# print(2<3)
+# name="aa"
+
+# print(type("呼呼"))
+# print(type(123))
+# print(type(12.12))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+
+# print(name)
+# a = input("请输入：") #a为字符串
+# b= input("请输入：")  #b为字符串
+# print("input获取内容",a+b) # 结果只是字符串的相加
+
+# a=int("233")
+# print(type(a))
+
+# a = float( input("请输入：")) #a为小数
+# b= float(input("请输入：")) #b为小数
+
+# print("input获取内容",a+b) # 结果是小数的相加
+
+# a = 'qweqweqweqwe'
+# print(len(a))
+
+a = input("请输入内容1:")
+b = input("请输入内容2:")
+print("输出:",len(a)+len(b))
